5532fc9e2285059e3b6d28beb4cb1559e4d515fe
Abhay Sharma <abhkum75@gmail.com>
2023-06-22 01:55:21 +0530

    day 2

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz,{super.key});
+
+  final void Function() startQuiz;
   @override
   Widget build(context) {
     return Center(
@@ -20,7 +23,9 @@ class StartScreen extends StatelessWidget {
             height: 30,
           ),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: () {
+              startQuiz();
+            },
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
             label: const Text("Start Quiz"),
             icon: const Icon(Icons.arrow_right_alt),
